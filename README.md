306af92496d4bb84688e3cbe8c3a361c48d5b5c2
Viktorianorth <mikhaylovanorth@gmail.com>
2024-10-04 14:34:29 +0300

    commit 4.10

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
 # Lesson1Python
-my_heigh = 169
-print('my_heigh')
--- lesson_1_task_5.py
+++ lesson_1_task_5.py
@@ -1,15 +1,32 @@
-def num():
-    print(8)
-    print(8)
-    print(0)
-    print(0)
-    print(5)
-    print(5)
-    print(5)
-    print(3)
-    print(5)
-    print(3)
-    print(5)
-num()
-    
-    
+# def num():
+#     print(8)
+#     print(8)
+#     print(0)
+#     print(0)
+#     print(5)
+#     print(5)
+#     print(5)
+#     print(3)
+#     print(5)
+#     print(3)
+#     print(5)
+
+
+# num()     
+
+
+def printMe(num):
+    print(num, end="")
+
+
+printMe(8)
+printMe(8)
+printMe(8)
+printMe(8)
+printMe(8)
+printMe(8)
+printMe(8)
+printMe(8)
+printMe(8)
+printMe(8)
+printMe(8)
