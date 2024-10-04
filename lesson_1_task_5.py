ad2cffbff6a81d8260dac4070102b51c1a0ee784
Viktorianorth <mikhaylovanorth@gmail.com>
2024-10-04 14:50:33 +0300

    исправление 4.10

--- lesson_1_task_5.py
+++ lesson_1_task_5.py
@@ -1,32 +1,15 @@
-# def num():
-#     print(8)
-#     print(8)
-#     print(0)
-#     print(0)
-#     print(5)
-#     print(5)
-#     print(5)
-#     print(3)
-#     print(5)
-#     print(3)
-#     print(5)
-
-
-# num()     
-
-
 def printMe(num):
     print(num, end="")
 
 
 printMe(8)
 printMe(8)
-printMe(8)
-printMe(8)
-printMe(8)
-printMe(8)
-printMe(8)
-printMe(8)
-printMe(8)
-printMe(8)
-printMe(8)
+printMe(0)
+printMe(0)
+printMe(5)
+printMe(5)
+printMe(5)
+printMe(3)
+printMe(5)
+printMe(3)
+printMe(5)
