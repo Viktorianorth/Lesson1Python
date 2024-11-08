daadc0d9070d0ba26ac22a8dd00fb4f966bf3f0d
Viktorianorth <mikhaylovanorth@gmail.com>
2024-11-08 22:32:17 +0300

    lesson6 change 8/11

--- lesson 06/waiting_a_picture.py
+++ lesson 06/waiting_a_picture.py
@@ -1,20 +1,31 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from webdriver_manager.chrome import ChromeDriverManager
 
+service = Service(ChromeDriverManager().install())
 driver = webdriver.Chrome()
 
 try:
     driver.get("https://bonigarcia.dev/selenium-webdriver-java/loading-images.html")
 
-    wait = WebDriverWait(driver, 15)
-    images = wait.until(EC.presence_of_all_elements_located((By.TAG_NAME, "img")))
+    WebDriverWait(driver, 30).until(
+        EC.text_to_be_present_in_element((By.ID, "text"), "Please wait until the images are loaded...")
+    )
+    print("Текст 'Please wait until the images are loaded...' обнаружен.")
 
-    print(f"Найдено изображений: {len(images)}")
+    WebDriverWait(driver, 30).until(
+        EC.text_to_be_present_in_element((By.ID, "text"), "Done!")
+    )
+    print("Текст изменился на 'Done!'")
 
-    if len(images) >= 3:
-        third_image = images[2]
-        src_value = third_image.get_attribute("src")
+    try:
+        image = driver.find_element(By.CSS_SELECTOR, '#award')
+        print("Изображение найдено:", image.get_attribute("src"))
+    except Exception:
+        print("Изображение с ID 'award' не найдено.")
+
 finally:
     driver.quit()
